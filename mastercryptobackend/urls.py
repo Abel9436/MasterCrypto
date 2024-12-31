6c16fe4cfd30fc9785393c3f2559d482dc914741
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2024-12-31 21:06:33 +0300

    Update urls.py

--- mastercryptobackend/urls.py
+++ mastercryptobackend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = [  
+    path('mastercryptoadmin123/', admin.site.urls),
     path('', include('mcapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
